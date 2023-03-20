39f97a9aee3042eeb801d277e6ac3d3ffb603ea1
GregMcC5 <gregmcc@umich.edu>
2023-03-20 11:19:54 -0400

    Remove rest files

--- METS_tag_extraction.py
+++ METS_tag_extraction.py
@@ -3,8 +3,8 @@ import zipfile as zf
 from bs4 import BeautifulSoup
 import os
 
-ph_tag_list = []
-ao_tag_list = []
+ph_tag_aggregator = [["tag", "package count", "total count", "min", "max"]]
+ao_tag_aggregator = [["tag", "package count", "total count", "min", "max"]]
 
 #navigate to external drive
 print(os.getcwd())
@@ -24,6 +24,7 @@ for package in os.listdir():
                 print(f"souped - {package}")
 
                 # -processHistoy
+                ph_tag_list = []
                 ph_pe = soup.find("ph:processHistory")
                 print("len of ph:", len(ph_pe))
                 print(ph_pe.name)
@@ -33,40 +34,72 @@ for package in os.listdir():
                         print('/'.join([parent.name for parent in item.find_parents()][::-1]) + f"/{item.name}")
                         ph_tag_list.append('/'.join([parent.name for parent in item.find_parents()][::-1]) + f"/{item.name}")
 
+                ph_tag_counts = [["tag", "package count", "total count"]]
+                for tag in ph_tag_list:
+                    print(tag)
+                    if tag in [x[0] for x in ph_tag_counts]:
+                        [x for x in ph_tag_counts if x[0] == tag][0][2] += 1
+                    else:
+                        ph_tag_counts.append([tag, 1, 1])
+
+                for entry in ph_tag_counts[1:]:
+                    if entry[0] in [x[0] for x in ph_tag_aggregator[1:]]:
+                        #presence in record
+                        [x for x in ph_tag_aggregator if x[0] == entry[0]][0][1] += 1
+                        #total count
+                        [x for x in ph_tag_aggregator if x[0] == entry[0]][0][2] += entry[2]
+                        #minimum
+                        if entry[2] < [x for x in ph_tag_aggregator if x[0] == entry[0]][0][3]:
+                            [x for x in ph_tag_aggregator if x[0] == entry[0]][0][3] = entry[2]
+                        #maximum
+                        if entry[2] > [x for x in ph_tag_aggregator if x[0] == entry[0]][0][4]:
+                            [x for x in ph_tag_aggregator if x[0] == entry[0]][0][4] = entry[2]
+
+                    else:
+                        ph_tag_aggregator.append(entry + [entry[2], entry[2]])
+
                 # -audioObject
+                ao_tag_list = []
                 ao = soup.find("aes:audioObject")
                 for item in ao.descendants:
                     if item.name is not None and item.is_empty_element is not True:
                         print('/'.join([parent.name for parent in item.find_parents()][::-1]) + f"/{item.name}")
                         ao_tag_list.append('/'.join([parent.name for parent in item.find_parents()][::-1]) + f"/{item.name}")
+
+                ao_tag_counts = [["tag", "package count", "total count"]]
+                for tag in ao_tag_list:
+                    print(tag)
+                    if tag in [x[0] for x in ao_tag_counts]:
+                        [x for x in ao_tag_counts if x[0] == tag][0][2] += 1
+                    else:
+                        ao_tag_counts.append([tag, 1, 1])
+
+                for entry in ao_tag_counts[1:]:
+                    if entry[0] in [x[0] for x in ao_tag_aggregator[1:]]:
+                       #presence in record
+                        [x for x in ao_tag_aggregator if x[0] == entry[0]][0][1] += 1
+                        #total count
+                        [x for x in ao_tag_aggregator if x[0] == entry[0]][0][2] += entry[2]
+                        #minimum
+                        if entry[2] < [x for x in ao_tag_aggregator if x[0] == entry[0]][0][3]:
+                            [x for x in ao_tag_aggregator if x[0] == entry[0]][0][3] = entry[2]
+                        #maximum
+                        if entry[2] > [x for x in ao_tag_aggregator if x[0] == entry[0]][0][4]:
+                            [x for x in ao_tag_aggregator if x[0] == entry[0]][0][4] = entry[2]
+
+                    else:
+                        ao_tag_aggregator.append(entry + [entry[2], entry[2]])
     except:
         continue
 
-# -tabulating proceessHistory tags
-ph_tag_counts = [["tag", "package count", "total count"]]
-
-for tag in ph_tag_list:
-    print(tag)
-    if tag in [x[0] for x in ph_tag_counts]:
-        [x for x in ph_tag_counts if x[0] == tag][0][2] += 1
-    else:
-        ph_tag_counts.append([tag, 1, 1])
-
-
-# -tabulating proceessHistory tags
-ao_tag_counts = [["tag", "package count", "total count"]]
-
-for tag in ao_tag_list:
-    if tag in [x[0] for x in ao_tag_counts]:
-        [x for x in ao_tag_counts if x[0] == tag][0][2] += 1
-    else:
-        ao_tag_counts.append([tag, 1, 1])
-
+for list in [ph_tag_aggregator, ao_tag_aggregator]:
+    for entry in list[1:]:
+        entry[2] = round(entry[2]/65, 1)
 
 # -Writing documents
 os.chdir("/Users/gregorymccollum/Documents/GitHub/AudioAssessment")
 
-mu.write_csv("ph_tag_tabulation.csv", ph_tag_counts)
-mu.write_csv("ao_tag_tabulation.csv", ao_tag_counts)
+mu.write_csv("ph_tag_tabulation.csv", ph_tag_aggregator)
+mu.write_csv("ao_tag_tabulation.csv", ao_tag_aggregator)
 
 print("done")
